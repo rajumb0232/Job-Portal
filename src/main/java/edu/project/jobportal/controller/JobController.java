e70331cc50cae992e5dd237b685a0c96f3346c9d
rajumb0232 <rajugowda0232@gmail.com>
2023-05-27 19:42:40 +0530

    created API endpoint for updating the Job

--- src/main/java/edu/project/jobportal/controller/JobController.java
+++ src/main/java/edu/project/jobportal/controller/JobController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,5 +39,11 @@ public class JobController {
 	public ResponseEntity<responseStructure<List<JobResponse>>> getJobsBySkill(
 			@RequestParam long skillId){
 		return jobService.getJobsBySkill(skillId);
+	}
+	
+	@PutMapping
+	public ResponseEntity<responseStructure<JobResponse>> updateJobById(
+			@RequestBody JobDto jobDto, @RequestParam int jobId){
+		return jobService.updateJobById(jobDto, jobId);
 	}
 }
